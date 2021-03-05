8bde8b5d1b308f24caf8146138a16ab780ed0377
xiejiaqi <xiejiaqi77.job@gmail.com>
2021-03-05 18:50:35 +0900

    modified -2

--- python_test_object-master/test.py
+++ python_test_object-master/test.py
@@ -16,17 +16,21 @@ N = len(data_wash)
 for item in data_wash:
     item[0] = int(item[0])
 
-# count for the situation that if no output
-count = 0
+
 m = data_wash[-1][0]
+
+# remove m
+data_wash = data_wash[:-1]
+
+# sorting data_wash for sはiの小さい順に出力してください(
+data_wash = sorted(data_wash, key=lambda data: data[0])
+# print(data_wash)
+res = ""
 for i in range(N-1):
     if m%data_wash[i][0] == 0:
-        count += 1
-        print(data_wash[i][1])
+        res += data_wash[i][1]
 
-# if no output then print m
-if count == 0:
-    print(m)
+print(res) if res else print(m)
 
 
 # print(data_wash)
